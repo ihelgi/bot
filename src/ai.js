925118f96040bb1323f68901824230751f404487
iHelgi <eliahungintonti@gmail.com>
2025-02-15 11:03:15 +0100

    latest version

--- src/ai.js
+++ src/ai.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const translate = require('@vitalets/google-translate-api');  // Aggiungi questa libreria per la traduzione
 
 async function handleAICommand(message) {
     try {
@@ -11,16 +10,8 @@ async function handleAICommand(message) {
             headers: { 'Authorization': `Bearer ${process.env.OPENROUTER_API_KEY}` }
         });
 
-        let reply = response.data.choices[0].message.content;
-
-        // Tradurre la risposta in italiano
-        translate(reply, { to: 'it' }).then(res => {
-            message.reply(res.text);  // Risposta tradotta in italiano
-        }).catch(err => {
-            console.error("Errore nella traduzione:", err);
-            message.reply("Errore nella traduzione!");
-        });
-        
+        const reply = response.data.choices[0].message.content;
+        message.reply(reply);
     } catch (error) {
         console.error("Errore AI:", error);
         message.reply("Non riesco a rispondere ora. Riprova più tardi!");
